f8adf602fc11b10ec20e2e28c846414177803095
Lamda <takatim1574@icloud.com>
2018-05-20 22:49:20 +0900

    Add files via upload

--- src/Mazai/Main.php
+++ src/Mazai/Main.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Mazai;
-
 /**
 * The MIT License
 * Copyright (c) 2018 MazaiCrafty
 */
+
+namespace Mazai;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
@@ -24,6 +24,11 @@ use pocketmine\utils\TextFormat as TF;
 
 use Mazai\command\TestCommand;
 
+use Mazai\event\Language;
+
+use jojoe77777\FormAPI\FormAPI;
+use onebone\economyapi\EconomyAPI;
+
 class Main extends PluginBase implements Listener{
 
     const VERSION = 1.0;
@@ -32,6 +37,9 @@ class Main extends PluginBase implements Listener{
 
     /** @var $this */
     private static $instance;
+
+    private $economy_api;
+    private $form_api;
     
     public function onLoad(): void{
         $this->getLogger()->info("プラグインを読み込みます...");
@@ -45,10 +53,14 @@ class Main extends PluginBase implements Listener{
         $this->getLogger()->info("プラグインを有効にします...");
 
         if(!file_exists($this->getDataFolder())){
-			$this->getLogger()->info("設定ファイルを生成します...");
+            $this->getLogger()->info("設定ファイルを生成します...");
 			@mkdir($this->getDataFolder() , 0777);
-			$this->saveResource("Config.yml");
-		}
+            $this->saveResource("Config.yml");
+        }
+
+        /** API */
+        $this->economy_api = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+        $this->form_api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
     }
 
     /**
@@ -72,15 +84,72 @@ class Main extends PluginBase implements Listener{
 
     public function onJoin(PlayerJoinEvent $event): void{
         $player = $event->getPlayer();
-        $lang = $player->getLocale();
+        $lang = Language::getLanguage($player);
         switch ($lang){
-            case "ja_JP":
-            $player->sendMessage("さてはオメー日本語使いだな");
+            case 'ja':
+            $player->sendMessage("ようこそ猫鯖へ"); // センスいいのでてこい
             break;
 
-            case "en_US":
-            $player->sendMessage("You use English");
+            case 'en':
+            $player->sendMessage("Welcome to NekoServer JP");
             break;
+
+            case 'ko':
+            $player->sendMessage("NekoServer JP에 오신 것을 환영합니다");
+            break;
+
+            case 'zh':
+            $player->sendMessage("欢迎来到NekoServer JP");
+            break;
+        }
+
+        foreach ($this->getServer()->getOnlinePlayers() as $players){
+            $event->setJoinMessage("");
+
+            switch ($lang){
+                case 'ja':
+                $players->sendMessage($player->getName() . "が世界にやってきた");
+                break;
+    
+                case 'en':
+                $players->sendMessage($player->getName() . " join the game");
+                break;
+
+                case 'ko':
+                $players->sendMessage($player->getName() . "는 게임에 참가했습니다");
+                break;
+
+                case 'zh':
+                $players->sendMessage($player->getName() . "加入了游戏");
+                break;
+            }
+        }
+    }
+
+    public function onQuit(PlayerQuitEvent $event): void{
+        $player = $event->getPlayer();
+        $lang = Language::getLanguage($player);
+
+        foreach ($this->getServer()->getOnlinePlayers() as $players){
+            $event->setQuitMessage("");
+
+            switch ($lang){
+                case 'ja':
+                $players->sendMessage($player->getName() . "が退出しました");
+                break;
+    
+                case 'en':
+                $players->sendMessage($player->getName() . " left the game");
+                break;
+
+                case 'ko':
+                $players->sendMessage($player->getName() . "가 퇴출했습니다");
+                break;
+
+                case 'zh':
+                $players->sendMessage($player->getName() . "已经离开了");
+                break;
+            }
         }
     }
 
@@ -95,8 +164,22 @@ class Main extends PluginBase implements Listener{
     }
 
     /**
-     * @return $this
-     */
+    * @return EconomyAPI 
+    */
+    public function getEconomyAPI(): EconomyAPI{
+        return $this->economy_api;
+    }
+
+    /**
+    * @return FormAPI 
+    */
+    public function getFormAPI(): FormAPI{
+        return $this->form_api;
+    }
+
+    /**
+    * @return $this
+    */
     public static function getInstance(){
         if (!self::$instance){
             self::$instance = new Main();
@@ -105,4 +188,3 @@ class Main extends PluginBase implements Listener{
 	}
 
 }
-
